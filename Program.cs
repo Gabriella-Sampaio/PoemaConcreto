00ce4daba5239330ae06b8a446d728e0608eac5b
Gabriella Sampaio <gabyy.t.s21@gmail.com>
2025-03-08 12:25:22 -0300

    Término

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine(@"
+                     se
+                     nasce
+                     morre nasce
+                     morre nasce morre
+                                    renasce remorre renasce
+                                            remorre renasce
+                                                    remorre
+                        re                               re
+                  desnasce
+         desmorre desnasce
+desmorre desnasce desmorre
+                          nascemorrenasce
+                          morrenasce
+                          morre
+                          se
+
+
+ - Haroldo de Campos
+  ""Nascemorre"" (1958)");
